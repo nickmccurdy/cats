7024cf19f16c0ea89f350dc1ce1872bcdde3d97d
Nicolas McCurdy <thenickperson@gmail.com>
2013-07-25 22:57:46 -0400

    Add Cats.fact

--- lib/cats.rb
+++ lib/cats.rb
@@ -1,7 +1,17 @@
+require 'json'
+require 'open-uri'
+
 module Cats
+
+  extend self
 
   VERSION = "0.0.1"
 
-  # Your code goes here...
+  def fact quantity=1
+    # number: Number of cat facts to retrieve (0-100).
+    url = "http://catfacts-api.appspot.com/api/facts?number=#{quantity}"
+    content = open(url).read
+    JSON.parse(content)['facts']
+  end
 
 end
