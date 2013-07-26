984dde40da35022123e2c06b6fd5b1b8d90721d3
Nicolas McCurdy <thenickperson@gmail.com>
2013-07-25 23:03:33 -0400

    Code style tweaks

--- lib/cats.rb
+++ lib/cats.rb
@@ -5,9 +5,9 @@ module Cats
 
   extend self
 
-  VERSION = "0.0.1"
+  VERSION = '0.0.1'
 
-  def fact quantity=1
+  def fact(quantity = 1)
     # number: Number of cat facts to retrieve (0-100).
     url = "http://catfacts-api.appspot.com/api/facts?number=#{quantity}"
     content = open(url).read
