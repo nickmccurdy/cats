9eaa6580ccebabe94f6242465a457bf33024f06f
Nicolas McCurdy <thenickperson@gmail.com>
2013-07-28 01:34:39 -0400

    Document ALL the things!

--- lib/cats.rb
+++ lib/cats.rb
@@ -1,26 +1,40 @@
 require 'thor'
 
+# The central module for Cats, a library/tool that does a small number of
+# cat-related tasks
 module Cats
 
   extend self
 
+  # The version of the Cats gem
   VERSION = '0.0.1'
 
+  # An Array of cat facts. Each string contains one fact. The facts are read
+  # from a simple text file, with one fact on each line.
   @facts = File.open("#{File.dirname __FILE__}/facts.txt").read.split(/\n/)
 
+  # Returns a given number of cat facts, which defaults to 1
+  #
+  # TODO: Consider allowing more than 202 cat facts (show duplicates?)
+  #
+  # @param [Fixnum] quantity the number of cat facts to recieve (0-many)
+  #
+  # @return [Array] an Array of Strings of cat facts
   def fact(quantity = 1)
-    # TODO: Consider allowing more than 202 cat facts (show duplicates?)
     @facts.sample quantity
   end
 
 end
 
+# The command line interface for Cats. Uses Thor to map each method to a
+# command.
 class Cats::CLI < Thor
 
   default_task :fact
 
   desc 'fact QUANTITY', 'Displays a given number of cat facts'
   argument :quantity, type: :numeric, default: 1
+  # Acts as the UI for Cats.fact by showing each cat fact on a new line of text
   def fact
     facts = Cats.fact quantity
     facts.each { |fact| puts fact }
