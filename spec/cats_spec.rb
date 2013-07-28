45f29a883e548dd9c16e4f6ca9c7548f2359f3d1
Nicolas McCurdy <thenickperson@gmail.com>
2013-07-28 02:46:50 -0400

    Add initial tests for Cats

---
+++ spec/cats_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Cats do
+  it 'should have a version number' do
+    ::Cats::VERSION.wont_be_nil
+  end
+
+  it 'should generate cat facts' do
+    Cats.fact(0).must_equal []
+
+    Cats.fact.length.must_equal 1
+    Cats.fact(1).length.must_equal 1
+    Cats.fact(10).length.must_equal 10
+
+    #Cats.fact(1000).length.must_equal 1000
+    Cats.fact 1000
+    
+    Cats.fact(10).each { |fact| fact.must_be_instance_of String }
+    
+    proc { Cats.fact(-1) }.must_raise ArgumentError
+  end
+end
